9d0e4c0e253ef25428178a6359e34acdc6ad7ef5
doctTYPE <84064501+doctTYPE@users.noreply.github.com>
2021-09-08 12:17:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const result = document.getElementById('text')
 let calculate = (num) =>{ 
   
   result.value += num;
-  tryy()
+ 
 };
 
 
@@ -17,19 +17,19 @@ let calculate = (num) =>{
 
 function cance(){
   result.value = result.value.slice(0 , -1)
-  tryy()
+  
 }
 
 
 function cancel() {
   // body...
   result.value = " "
-  tryy()
+  
 }
 function egual(){
   try{
     result.value = eval(result.value)
-    tryy()
+    
     
   }
   catch(err){
@@ -37,11 +37,3 @@ function egual(){
   }
 }
 
-function tryy(){
-  if(result.value.length>11){
-    alert("try to use 11 caracters")
-  }
-  else{
-    pass
-  }
-}
